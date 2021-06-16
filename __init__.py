968d50c527c39eb6025beb39ef1393a28db3e3ac
Lorenzo Squadrani <lorenzo.squadrani@gmail.com>
2021-06-17 00:14:19 +0200

    Removed duplicated files (created during re-organization of last commit)

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .model import RobertasTree
-from .dataset import RobertasTreeDataset
-from .inferators import RobertasTreeInferator
-
-__author__  = ['Lorenzo Squadrani']
-__email__ = ['lorenzo.squadrani@studio.unibo.it']
